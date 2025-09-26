63a4556adf0bcb9e740cba02c9c132e9dae575fe
Arfat Irfan Naik <arfatnaik05@gmail.com>
2025-09-26 23:13:01 +0000

    Delete all files from t1 branch

--- iot-system/control-system/control_system.py
+++
@@ -1,6 +0,0 @@
-# Control System placeholder
-print("Control System service starting...")
-import time
-while True:
-    time.sleep(60)
-    print("Control System running...")
--- iot-system/ml-engine/ml_engine.py
+++
@@ -1,6 +0,0 @@
-# ML Engine placeholder
-print("ML Engine service starting...")
-import time
-while True:
-    time.sleep(60)
-    print("ML Engine running...")
